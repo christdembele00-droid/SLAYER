46b3026591f381c573940ce10960ddfa1daf558f
christdembele00-droid <christdembele00@gmail.com>
2026-09-21 23:57:59 +0000

    Phase 2: add PlayerController.ts

---
+++ src/player/PlayerController.ts
@@ -0,0 +1,23 @@
+import { PlayerData, PlayerIntent, PlayerRuntimeState } from "./PlayerTypes";
+import { PlayerMovement } from "./PlayerMovement";
+
+export class PlayerController {
+  readonly movement = new PlayerMovement();
+
+  createState(data: PlayerData, position = { x: 0, y: 0, z: 0 }): PlayerRuntimeState {
+    return {
+      position: { ...position },
+      velocity: { x: 0, y: 0, z: 0 },
+      rotationY: 0,
+      locomotion: "Idle",
+      ballMode: "NoBall",
+      action: "None",
+      stamina: data.physical.stamina
+    };
+  }
+
+  update(data: PlayerData, state: PlayerRuntimeState, intent: PlayerIntent, delta: number): void {
+    this.movement.update(data, state, intent, delta);
+    state.action = intent.action;
+  }
+}
