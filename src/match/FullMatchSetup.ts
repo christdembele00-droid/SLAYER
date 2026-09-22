21619f816bb55c66c0f5768f8811851183d94576
christdembele00-droid <christdembele00@gmail.com>
2026-09-22 00:04:55 +0000

    SLAYER: full match and AI src/match/FullMatchSetup.ts

---
+++ src/match/FullMatchSetup.ts
@@ -0,0 +1,4 @@
+import {PlayerSystem} from "../player/PlayerSystem";import {PlayerFactory} from "../player/PlayerFactory";import {PlayerPosition} from "../player/PlayerTypes";import {HOME_TEAM,AWAY_TEAM} from "./TeamData";
+const slots:PlayerPosition[]=["GK","LB","CB","CB","RB","DM","CM","AM","LW","RW","ST"];
+export class FullMatchSetup{populate(ps:PlayerSystem){if(ps.all().length>=22)return;for(const [team,data] of [[HOME_TEAM,[
+[-34,-49],[-17,-39],[-5,-43],[8,-43],[25,-39],[-12,-25],[4,-25],[0,-10],[-25,-18],[25,-18],[0,-5]],[AWAY_TEAM,[[-34,49],[-25,39],[-8,43],[5,43],[22,39],[-4,25],[12,25],[0,10],[-25,18],[25,18],[0,5]]]] as const){for(let i=0;i<11;i++){const d=PlayerFactory.createPlayerData({playerId:team.id+"-"+(i+1),name:team.name+" "+(i+1),position:slots[i],teamId:team.id});const p=ps.create(d);p.state.position={x:data[i][0],y:0.0,z:data[i][1]};}}}}
