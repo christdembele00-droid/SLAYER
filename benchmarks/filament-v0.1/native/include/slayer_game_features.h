ba30f8bc45e05089376ecf8524bbe9891c2e2126
christdembele00-droid <christdembele00@gmail.com>
2026-09-22 04:32:54 +0000

    feat: add native 22-player gameplay foundation

---
+++ benchmarks/filament-v0.1/native/include/slayer_game_features.h
@@ -0,0 +1,16 @@
+#pragma once
+namespace slayer::features {
+constexpr bool gpuSkinning=true;
+constexpr bool pbrTerrain=true;
+constexpr bool hdrIbl=true;
+constexpr bool dynamicShadows=true;
+constexpr bool taa=true;
+constexpr bool aces=true;
+constexpr bool dynamicResolution=true;
+constexpr bool ballMagnus=true;
+constexpr bool playerFatigue=true;
+constexpr bool offside=true;
+constexpr bool tacticalAI=true;
+constexpr bool fixedPhysics8ms=true;
+constexpr int playersPerMatch=22;
+}
