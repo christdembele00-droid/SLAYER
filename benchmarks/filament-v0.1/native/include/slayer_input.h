3a8201f872545b58de77ad2f31d949277599a8bd
christdembele00-droid <christdembele00@gmail.com>
2026-09-22 04:56:38 +0000

    feat(game): expose native settings bridge

--- benchmarks/filament-v0.1/native/include/slayer_input.h
+++ benchmarks/filament-v0.1/native/include/slayer_input.h
@@ -9,6 +9,7 @@ void slayer_game_update(float dt);
 void slayer_game_set_input(float move_x,float move_y,float pass,float shoot,float sprint,float tackle,int selected);
 void slayer_game_get_transforms(SlayerTransform* out,uint32_t capacity);
 void slayer_game_get_score(uint32_t* home,uint32_t* away,uint32_t* seconds);
+void slayer_game_set_settings(const slayer::SlayerSettings& settings);
 #ifdef __cplusplus
 }
 #endif
