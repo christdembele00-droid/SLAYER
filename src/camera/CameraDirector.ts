0c7ee3fbb794d76c4a9042214fa09135e2830f25
christdembele00-droid <christdembele00@gmail.com>
2026-09-22 00:12:24 +0000

    fix: type camera event listener correctly

--- src/camera/CameraDirector.ts
+++ src/camera/CameraDirector.ts
@@ -1,2 +1,11 @@
-import * as THREE from "three"; import { MatchEvent } from "../core/MatchEvents";
-export class CameraDirector{private target=new THREE.Vector3();update(camera:THREE.PerspectiveCamera,ball:{x:number;y:number;z:number}){this.target.set(ball.x,0,ball.z);camera.position.lerp(new THREE.Vector3(ball.x,18,ball.z+28),.08);camera.lookAt(this.target);}onEvent(_event:MatchEvent<keyof any>){/* presentation hooks for goal/save/replay */}}
+import * as THREE from "three";
+import { MatchEvent, MatchEventName } from "../core/MatchEvents";
+export class CameraDirector{
+  private target=new THREE.Vector3();
+  update(camera:THREE.PerspectiveCamera,ball:{x:number;y:number;z:number}){
+    this.target.set(ball.x,0,ball.z);
+    camera.position.lerp(new THREE.Vector3(ball.x,18,ball.z+28),.08);
+    camera.lookAt(this.target);
+  }
+  onEvent(_event:MatchEvent<MatchEventName>){/* event-driven presentation hook */}
+}
