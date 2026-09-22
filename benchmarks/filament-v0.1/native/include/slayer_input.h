4d53a3bf9919d67ba9447c78c6a7cbbe0a00a136
christdembele00-droid <christdembele00@gmail.com>
2026-09-22 04:32:46 +0000

    feat: add native 22-player gameplay foundation

---
+++ benchmarks/filament-v0.1/native/include/slayer_input.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <cstdint>
+#include "slayer_game.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
+void slayer_game_reset();
+void slayer_game_update(float dt);
+void slayer_game_set_input(float move_x,float move_y,float pass,float shoot,float sprint,float tackle,int selected);
+void slayer_game_get_transforms(SlayerTransform* out,uint32_t capacity);
+void slayer_game_get_score(uint32_t* home,uint32_t* away,uint32_t* seconds);
+#ifdef __cplusplus
+}
+#endif
